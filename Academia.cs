7da48be62dad32e33bdbc9327feef0528034bfb0
Carol <carolnerisfonseca@gmail.com>
2023-11-28 22:06:04 -0300

    Resolvido todos os bugs não atendidos em sala

--- Academia.cs
+++ Academia.cs
@@ -1,1 +1,76 @@
-namespace academia;
+using System;
+using System.Collections.Generic;
+using Pessoas;
+
+namespace Academia
+{
+    class Metodos
+    {
+        protected List<(object pessoa, string tipo)> cadastros = new List<(object, string)>();
+
+        public void CadastrarCliente(string nome, DateTime dataNascimento, string cpf, double altura, double peso)
+        {
+            Cliente cliente = new Cliente
+            {
+                Nome = nome,
+                DataNascimento = dataNascimento,
+                CPF = cpf,
+                Altura = altura,
+                Peso = peso
+            };
+
+            cadastros.Add((cliente, "Cliente"));
+        }
+
+        public void CadastrarTreinador(string nome, DateTime dataNascimento, string cpf, string cref)
+        {
+            Treinador treinador = new Treinador
+            {
+                Nome = nome,
+                DataNascimento = dataNascimento,
+                CPF = cpf,
+                CREF = cref
+            };
+
+            cadastros.Add((treinador, "Treinador"));
+        }
+
+        public void ExibirCadastros()
+        {
+            foreach (var cadastro in cadastros)
+            {
+                Console.WriteLine($"Tipo: {cadastro.tipo}");
+                
+                if (cadastro.tipo == "Cliente")
+                {
+                    var cliente = (Cliente)cadastro.pessoa;
+                    ExibirDetalhesCliente(cliente);
+                }
+                else if (cadastro.tipo == "Treinador")
+                {
+                    var treinador = (Treinador)cadastro.pessoa;
+                    ExibirDetalhesTreinador(treinador);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private void ExibirDetalhesCliente(Cliente cliente)
+        {
+            Console.WriteLine($"Nome: {cliente.Nome}");
+            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento}");
+            Console.WriteLine($"CPF: {cliente.CPF}");
+            Console.WriteLine($"Altura: {cliente.Altura}");
+            Console.WriteLine($"Peso: {cliente.Peso}");
+        }
+
+        private void ExibirDetalhesTreinador(Treinador treinador)
+        {
+            Console.WriteLine($"Nome: {treinador.Nome}");
+            Console.WriteLine($"Data de Nascimento: {treinador.DataNascimento}");
+            Console.WriteLine($"CPF: {treinador.CPF}");
+            Console.WriteLine($"CREF: {treinador.CREF}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,55 +1,20 @@
-﻿using Sytem;
-using academia;
-using Sytem.Collections.Generic
+﻿using System;
+using Pessoas; // Importando o namespace onde as classes Pessoa, Cliente e Treinador estão definidas
+using Academia; // Importando o namespace onde a classe Metodos está definida
 
-
-    class Program
+class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Academia academia = new Academia();
+        Metodos metodos = new Metodos(); // Instanciando a classe Metodos para utilizarmos seus métodos
 
-            // Adicionando treinadores
-            Treinador treinador1 = new Treinador
-            {
-                Nome = "João Silva",
-                DataNascimento = new DateTime(1985, 5, 15),
-                CPF = "12345678901",
-                CREF = "ABC123"
-            };
+        // Exemplo de cadastro de um cliente
+        metodos.CadastrarCliente("João", new DateTime(1990, 5, 15), "12345678900", 1.75, 70.5);
 
-            Treinador treinador2 = new Treinador
-            {
-                Nome = "Maria Santos",
-                DataNascimento = new DateTime(1990, 10, 25),
-                CPF = "98765432109",
-                CREF = "DEF456"
-            };
+        // Exemplo de cadastro de um treinador
+        metodos.CadastrarTreinador("Maria", new DateTime(1985, 9, 20), "98765432100", "123456");
 
-            academia.AdicionarTreinador(treinador1);
-            academia.AdicionarTreinador(treinador2);
-
-            // Adicionando clientes
-            Cliente cliente1 = new Cliente
-            {
-                Nome = "Pedro Souza",
-                DataNascimento = new DateTime(1998, 3, 8),
-                CPF = "11122233344",
-                Altura = 1.75,
-                Peso = 70
-            };
-
-            Cliente cliente2 = new Cliente
-            {
-                Nome = "Ana Oliveira",
-                DataNascimento = new DateTime(1995, 12, 12),
-                CPF = "55566677788",
-                Altura = 1.60,
-                Peso = 55
-            };
-
-            academia.AdicionarCliente(cliente1);
-            academia.AdicionarCliente(cliente2);
-        }
+        // Exibindo os cadastros realizados
+        metodos.ExibirCadastros();
     }
 }
